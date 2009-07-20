b11862d08f8e70df03bab455f4def28c72930813
zhongxingdou <zhongxingdou@gmail.com>
2009-07-20 11:41:43 +0800

    updated find tips order by

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @tips = Tip.find(:all, :conditions => {:hidden => false}, :order => "top ASC, id DESC")
+    @tips = Tip.find(:all, :conditions => {:hidden => false}, :order => "top DESC, id DESC")
   end
 end

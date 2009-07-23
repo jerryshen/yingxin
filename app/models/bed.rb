427e91d4004a59ab39973c48ca8e465527a5a8fe
zhongxingdou <zhongxingdou@gmail.com>
2009-07-24 01:22:03 +0800

    updated bed_asigning views

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,4 +1,31 @@
 class Bed < ActiveRecord::Base
-  belongs_to :room
-  belongs_to :student
+	belongs_to :room
+	belongs_to :student
+
+	validates_presence_of :room_id, :number
+	before_save :validates_roommate_gender, :validates_bed_number
+
+	private
+	def validates_roommate_gender
+		r_gender = self.room.room_type == 0 ? '男生' : '女生' 
+		if self.student 
+			b_gender = self.student.gender == 'm' ? '男生' : '女生' 
+			if b_gender != r_gender
+				raise "警告，#{b_gender}不能入住#{r_gender}宿舍！"
+			end
+		end
+	end
+
+	def validates_bed_number
+		bed_count = self.room.bed_count
+		unless bed_count
+			raise "请先为床位所在的房间设置床位总数"
+		end
+		unless (1..bed_count).include? self.number
+			raise "床位编号(number)只能在1..#{bed_count}之间"
+		end
+		if self.room.beds.collect{|x| x.number if x != self}.include?(self.number)
+			raise "#{self.room.name}宿舍已经存在编号为#{self.number}的床位"
+		end
+	end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,9 @@ class Room < ActiveRecord::Base
   belongs_to :master, :class_name => "Student", :foreign_key => "student_id"
   belongs_to :info_class
   has_many :beds
-  
+  has_many :roommates, :through => :beds, :source => :student
+  before_save :validates_master_from_roommates
+
   def genarate_bed
     self.bed_count.times do |no|
       bed = self.beds.new
@@ -15,4 +17,13 @@ class Room < ActiveRecord::Base
       end
     end
   end
+
+  private
+  	def validates_master_from_roommates
+		if self.master
+			unless self.roommates.include?(self.master)
+				raise "舍长必须是宿舍成员之一"
+			end
+		end
+	end
 end
